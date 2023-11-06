b7432175b7fbd612db2c098164e79754bb1965d0
Xnern <berthaud6u@nathan@univ-lorraine.fr>
2023-11-06 16:49:50 +0100

    Petite mise a jour du web.php

--- routes/web.php
+++ routes/web.php
@@ -24,71 +24,71 @@ use Illuminate\Support\Facades\Route;
 
 
 
-//-------route combine--------
+//------- Route Combine --------//
 
-//route home
+// Route home
 Route::get('/', [IndexController::class, 'home'])->name('home');
-
-//route about cogagne
 Route::get('/presentation', [IndexController::class, 'about'])->name('about');
-
-//route contact
+Route::get('/faq', [IndexController::class, 'faq'])->name('faq');
 Route::get('/contact', [IndexController::class, 'contact'])->name('contact');
 
-//route for all product
+// Route products
 Route::get('/produits', [ProductController::class, 'index'])->name('products.index');
 
-//route conection
+// Route auth
 Route::get('/connexion', [AuthController::class, 'login'])->name('login');
 Route::post('/connexion', [AuthController::class, 'authenticate'])->name('login.authenticate');
-
-//route inscription
 Route::get('/inscription', [AuthController::class, 'register'])->name('register');
 Route::post('/inscription/cree', [AuthController::class, 'create'])->name('register.add');
 
-//route faq
-Route::get('/faq', [IndexController::class, 'faq'])->name('faq');
 
-//route for subsciption
+// Route subscription
 Route::get('/abonnement', [SubscriptionController::class, 'subscription'])->name('subscription');
 Route::post('/abonnement', [SubscriptionController::class, 'subscription'])->name('subscription.add');
 
-//-------route  user--------
+//------- Route User --------//
 
-//route setting
-Route::get('/parametre', [UserController::class, 'setting'])->name('setting');
-
-//route membership
-Route::get('/Adhesion', [SubscriptionController::class, 'membership'])->name('membership');
-Route::post('/Adhesion', [SubscriptionController::class, 'membership'])->name('membership.add');
-
-//route history of order
-Route::get('/historique', [UserController::class, 'history'])->name('history');
-//route calendar of delivery
-Route::get('/calendrier', [CalendarController::class, 'calendar'])->name('calendar');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
 
+    Route::get('/parametre', [UserController::class, 'setting'])->name('dashboard.setting');
 
-//--------route  admin--------
+    Route::get('/adhesion', [SubscriptionController::class, 'membership'])->name('dashboard.membership');
+    Route::post('/adhesion', [SubscriptionController::class, 'membership'])->name('dashboard.membership.add');
+
+    Route::get('/historique', [UserController::class, 'history'])->name('dashboard.history');
+    Route::get('/calendrier', [CalendarController::class, 'calendar'])->name('dashboard.calendar');
+});
+
+
+
+//-------- Route Admin --------//
+
+//TODO: Middleware admin
+
+
 //route setting
 Route::get('/parametre', [AdminController::class, 'account'])->name('account');
 
 //route visulisation of delivery turn
-Route::get('/visualisationDelivery', [DeliveryController::class, 'visualisation'])->name('delivery.visualisation');
+Route::get('/livraison/suivi', [DeliveryController::class, 'tracking'])->name('delivery.tracking');
 
-//route modification of delivery turn
-Route::get('/modificationDelivery', [DeliveryController::class, 'modification'])->name('delivery.modification');
-Route::post('/modificationDelivery', [DeliveryController::class, 'modification'])->name('delivery.modification.add');
+//route edit of delivery turn
+Route::get('/livraison/modifier', [DeliveryController::class, 'edit_view'])->name('delivery.edit-view');
+Route::post('/livraison/modifier', [DeliveryController::class, 'edit'])->name('delivery.edit');
 
 //route add product
-Route::get('/ajouterProduits', [ProductController::class, 'add'])->name('products.add');
-Route::post('/ajouterProduits', [ProductController::class, 'add'])->name('products.add.add');
+Route::get('/produit/ajouter', [ProductController::class, 'create_view'])->name('products.create-view');
+Route::post('/produit/ajouter', [ProductController::class, 'create'])->name('products.create');
 
 //route delete product
-Route::post('/ajouterProduits', [ProductController::class, 'add'])->name('products.delete');
+Route::post('/produit/supprimer', [ProductController::class, 'delete'])->name('products.delete');
 
-//route modification of product
-Route::post('/modificationProduits', [ProductController::class, 'modification'])->name('products.modification');
+//route edit of product
+Route::post('/produit/modifier', [ProductController::class, 'edit'])->name('products.edit');
 //route summary of orders to prepare and deliver
 Route::get('/resume', [DeliveryController::class, 'summary'])->name('summary');
 
@@ -99,10 +99,6 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+
 
 require __DIR__.'/auth.php';
