a78203cde6d9a8e1b351c9bf43ae0177bc360c9c
Xnern <berthaud6u@nathan@univ-lorraine.fr>
2024-01-15 14:07:38 +0100

    ajout reset bouton cancel delete modal

--- resources/views/staffs/edit-calendar.blade.php
+++ resources/views/staffs/edit-calendar.blade.php
@@ -150,7 +150,7 @@
                 <button type="submit" class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring focus:border-red-700 sm:ml-3 sm:w-auto sm:text-sm mr-4">
                     Supprimer
                 </button>
-                <button @click="closeDeleteModal" class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring focus:border-blue-300 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">
+                <button  type="reset" @click="closeDeleteModal" class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring focus:border-blue-300 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">
                     Annuler
                 </button>
             </div>
@@ -190,9 +190,6 @@
             },
             closeDeleteModal() {
                 this.showDeleteModal = false;
-            },
-            deleteLivraison() {
-                this.closeDeleteModal();
             }
 
 
